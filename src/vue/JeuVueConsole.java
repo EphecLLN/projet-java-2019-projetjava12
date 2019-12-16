7f1e7ca7ecfd486efd18130d253c8437962beebd
PATRICKTCHOUPE2TL1 <37470814+PATRICKTCHOUPE2TL1@users.noreply.github.com>
2019-12-16 22:10:37 +0100

    console change

--- src/vue/JeuVueConsole.java
+++ src/vue/JeuVueConsole.java
@@ -8,46 +8,67 @@ import controller.*;
 public class JeuVueConsole extends JeuVue implements Observer {
 	//implementer la vue console 
 	private String rep=" ";
+	
 	public JeuVueConsole(Jeu model, JeuController controller) throws ArithmeticException, IOException {
 		super(model, controller);
-		initMotus();
+		//initMotus(); 
 	}
+	
 	public void initMotus() throws IOException {
 		affiche("*********************BIENVENUE DANS LE JEU MOTUS***********************\n");
 		affiche("\tVeuillez entrer votre pseudo");
 		Scanner sc = new Scanner(System.in);
-		ctrl.setPseudo(sc.nextLine());
-		affiche("\nC'est Parti "+ctrl.getPseudo()+"!");
+		model.getJoueur().setPseudo(sc.nextLine());
+		affiche("\nC'est Parti "+model.getJoueur().getPseudo()+"!");
 		do {
 		this.deroulement();
 		affiche("Voulez-vous reessayer ? O/N ");
 		rep=sc.nextLine();
-		ctrl.setEssaisRestants(5);
+		model.getJoueur().setEssaisRestants(5);
 		}while(rep.equals("O") || rep.equals("o"));
 	}
+	
 	/**
 	 * Cette methode generalise le deroulement de la partie pour un joueur pour l'instant.
 	 * @throws IOException 
 	 */
 	public void deroulement() throws IOException {
 		Scanner sc = new Scanner(System.in);
-		ctrl.genererMotCorrect();
-		ctrl.setMotActuel();
+		boolean length = false;
+		model.genererMotCorrect();
+		model.updateActuel();
 		do {
-			affiche("Mot à deviner : "+ ctrl.getMotActuel()+" ("+ctrl.getMotCorrect().getLongueur()+" lettres )");
-			affiche("Il vous reste "+ctrl.getEssaisRestants()+" essais");
-			affiche("Votre proposition : ");
-			ctrl.setProposition(sc.nextLine());
-			ctrl.TraitementPropo(ctrl.getProposition());
-		}while(ctrl.getEssaisRestants()>0 && ctrl.isTrouve()!=true);		
-		if(ctrl.isTrouve()==false)
-			affiche("Dommage,le mot a trouver était "+ctrl.getMotCorrect().getValeur());
+			affiche("Mot à deviner : "+ model.getMotActuel()+" ("+model.getMotCorrect().getLongueur()+" lettres )");
+			affiche("Il vous reste "+model.getJoueur().getEssaisRestants()+" essais");
+			
+			do{
+				String n = new String();
+				affiche("Votre proposition : ");
+				n=sc.nextLine();
+				length = ctrl.setProposition(n);
+			}while(length==false);
+			
+			ctrl.traitementPropo(new Mot(model.getProposition()));
+			
+		}while(model.getJoueur().getEssaisRestants()>0 && model.isMotTrouve()!=true);
+			if(model.isMotTrouve()==false)
+			affiche("Dommage,le mot à trouver était "+model.getMotCorrect().getValeur());
 	}
 	public void update(Observable o, Object arg) {
-		Jeu p = (Jeu) o;
-		affiche(p.toString());
+	
 	}
 	public void affiche(String s) {
 		System.out.println(s);
+	}
+
+	@Override
+	public void enableWarning() {
+		affiche("Entrez un mot de 5 lettres");
+	}
+
+	@Override
+	public void disableWarning() {
+		// TODO Auto-generated method stub
+		
 	}
 }
