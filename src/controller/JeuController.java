38e29a4ef068fb0f80c802be6293c5b45db7deb3
PATRICKTCHOUPE2TL1 <37470814+PATRICKTCHOUPE2TL1@users.noreply.github.com>
2019-12-16 22:11:34 +0100

    controleur nouveau role

--- src/controller/JeuController.java
+++ src/controller/JeuController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import model.*;
 import vue.*;
 
+//notre controleur ici se charge de verifier que le mot proposer est bien conforme à nos spécifications
 public class JeuController {
 	Jeu model;
 	JeuVue view;
@@ -14,57 +15,47 @@ public class JeuController {
 		this.model=model;
 	}
 	
-	
-	
-	public void TraitementPropo(Mot mot) {
-		try {
-			model.TraitementProposition(mot);
-		}
-		catch (IOException e)
+	public boolean traitementPropo(Mot mot) throws IOException {
+			return model.TraitementProposition(mot);
+	}
+	public void updateMotActuel() {
+		model.updateActuel();
+	}
+	private boolean testerSaisie(String s){
+		boolean bonneLongueur=false;
+		
+		if(s.length()!=5)
+			bonneLongueur=false;
+		else
 		{
-			e.printStackTrace();
+			boolean isChar=true;
+			int pos=0;
+		while(pos < 5 && isChar)
+		{
+			isChar=Character.isLetter(s.charAt(pos)); 
+			pos++;
 		}
+			bonneLongueur=isChar;
+		}	
+		return bonneLongueur ;
 	}
-	public void setProposition(String s) {
-		this.model.getJoueur().setProposition(new Mot(s));
-	}
-	public Mot getProposition() {
-		return this.model.getJoueur().proposerMot();
-	}
-	public int getLongueurMot() {
-		return model.getMotCorrect().getLongueur();
-	}
-	public void setPseudo(String s) {
-		this.model.getJoueur().setPseudo(s);
-	}
-	public String getMotActuel() {
-		return this.model.getMotActuel();
-	}
-	public String getPseudo() {
-		return model.getJoueur().getPseudo();
-	}
-	public void setMotActuel() {
-		this.model.setMotActuel();
-	}
-	public void genererMotCorrect() {
-		model.genererMotCorrect();
-	}
-	public Mot getMotCorrect() {
-		return model.getMotCorrect();
-	}
-	public int getEssaisRestants() {
-		return model.getJoueur().getEssaisRestants();
-	}
-	public void setEssaisRestants(int i) {
-		model.getJoueur().setEssaisRestants(i);
-	}
-	public boolean isTrouve() {
-		return model.isMotTrouve();
+	
+	public boolean setProposition(String s) {
+		boolean prop=false;
+		
+			if(this.testerSaisie(s)==false) {
+				view.affiche("Entrez un mot de 5 lettres");
+				}
+			else {
+				model.setProposition(s);
+				prop=true;
+			}
+			
+			return prop;
 	}
 	public void addView(JeuVue view) {
 		this.view=view;
 	}
-
 	@Override
 	public String toString() {
 		return model.toString();
