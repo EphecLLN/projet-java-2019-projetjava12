828f8c694f5b181199d4e032c21a6bb16c3e5b5d
PATRICKTCHOUPE2TL1 <37470814+PATRICKTCHOUPE2TL1@users.noreply.github.com>
2019-12-17 01:31:57 +0100

    console change

--- src/vue/JeuVueConsole.java
+++ src/vue/JeuVueConsole.java
@@ -7,33 +7,46 @@ import controller.*;
 
 public class JeuVueConsole extends JeuVue implements Observer {
 	//implementer la vue console 
-	private String rep=" ";
+	private Scanner sc;
 	
 	public JeuVueConsole(Jeu model, JeuController controller) throws ArithmeticException, IOException {
 		super(model, controller);
-		//initMotus(); 
-	}
-	
-	public void initMotus() throws IOException {
 		affiche("*********************BIENVENUE DANS LE JEU MOTUS***********************\n");
 		affiche("\tVeuillez entrer votre pseudo");
-		Scanner sc = new Scanner(System.in);
-		model.getJoueur().setPseudo(sc.nextLine());
-		affiche("\nC'est Parti "+model.getJoueur().getPseudo()+"!");
-		do {
-		this.deroulement();
-		affiche("Voulez-vous reessayer ? O/N ");
-		rep=sc.nextLine();
-		model.getJoueur().setEssaisRestants(5);
-		}while(rep.equals("O") || rep.equals("o"));
+		update(null, null);
+		sc = new Scanner(System.in);
+		new Thread (new ReadInput()).start();	
 	}
-	
+	private class ReadInput implements Runnable{
+		public void run() {
+			while(true){
+				try{
+					String c = sc.nextLine();
+					model.getJoueur().setPseudo(c);
+					affiche("\nC'est Parti "+model.getJoueur().getPseudo()+"!");
+					String rep=" ";
+					do {
+						deroulement();
+						affiche("Voulez-vous reessayer ? O/N ");
+						rep=sc.nextLine();
+						model.getJoueur().setEssaisRestants(5);
+					}while(rep.equals("O") || rep.equals("o"));
+				}
+				catch(InputMismatchException e){
+					affiche("Format d'input incorrect");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 	/**
 	 * Cette methode generalise le deroulement de la partie pour un joueur pour l'instant.
 	 * @throws IOException 
 	 */
 	public void deroulement() throws IOException {
-		Scanner sc = new Scanner(System.in);
 		boolean length = false;
 		model.genererMotCorrect();
 		model.updateActuel();
@@ -51,8 +64,11 @@ public class JeuVueConsole extends JeuVue implements Observer {
 			ctrl.traitementPropo(new Mot(model.getProposition()));
 			
 		}while(model.getJoueur().getEssaisRestants()>0 && model.isMotTrouve()!=true);
+		
 			if(model.isMotTrouve()==false)
-			affiche("Dommage,le mot à trouver était "+model.getMotCorrect().getValeur());
+				affiche("Dommage,le mot à trouver était "+model.getMotCorrect().getValeur());
+			else 
+				affiche("Bravo vous avez trouvé le mot.");
 	}
 	public void update(Observable o, Object arg) {
 	
@@ -61,14 +77,4 @@ public class JeuVueConsole extends JeuVue implements Observer {
 		System.out.println(s);
 	}
 
-	@Override
-	public void enableWarning() {
-		affiche("Entrez un mot de 5 lettres");
-	}
-
-	@Override
-	public void disableWarning() {
-		// TODO Auto-generated method stub
-		
-	}
 }
